036bdeb5b8cd7d047f58860a13337acfcea2a3e5
Rebeca Carvalho <rebecactrindade@gmail.com>
2025-03-25 16:31:13 -0300

    Cria um arquivo sobre concatenação

---
+++ aula02/concatenar.php
@@ -0,0 +1,15 @@
+<?php
+    Echo "hello world!<br>";
+    echo "hello world!<br>";
+    Echo "hello world!<br>";
+
+    $nome = "Rebeca";
+    $sobrenome = "Trindade";
+    $numero = 8;
+
+    echo $nome . "lindo" . "<br>";
+    echo "${nome} <h1> ${sobrenome} </h1>";
+    echo $numero . "<p> " . $nome .
+    " " . $sobrenome . "</p>";
+?>
+    
