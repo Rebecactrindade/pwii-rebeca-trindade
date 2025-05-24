3d28b362a5a6c243e5183c0bac31b0fe381974a6
Rebeca Trindade <rebecactrindade@gmail.com>
2025-05-24 12:48:46 -0300

    exercícios em php

---
+++ exercicios/exercicio4.php
@@ -0,0 +1,7 @@
+<?php
+$valor = 200;
+$valortotal = $valor * 1.16;
+$parcela = $valortotal / 10;
+echo "o valor total com o acréscimo é: R$ $valortotal";
+echo "o valor da parcela é: R$ $parcela";
+?>
